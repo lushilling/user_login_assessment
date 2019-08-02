a8390bcc439617c5e57574586b5e695fe4a63a56
lushilling <lshilling@hotmail.co.uk>
2019-08-02 14:22:41 +0100

    all current tests work

--- src/_test_/NavigationBar.test.js
+++ src/_test_/NavigationBar.test.js
@@ -4,7 +4,8 @@ import renderer from 'react-test-renderer';
 import ReactDOM from 'react-dom';
 
 import NavigationBar from '../Components/NavigationBar';
-
+import { NavBar } from '../Components/NavigationBar';
+import Homepage from '../Components/Homepage';
 
 it('renders App', () => {
     const tree = renderer.create(<NavigationBar />).toJSON()
@@ -20,3 +21,9 @@ it('renders navigation bar without crashing', () => {
     ReactDOM.render(<NavigationBar />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders NavBar component', () => {
+    const wrapper = shallow(<Homepage />);
+    const content = <NavBar />;
+    expect(wrapper.contains(content)).toEqual(true);
+  });
